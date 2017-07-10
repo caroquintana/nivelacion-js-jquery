8c7547d3bb20854b5be5139e6486698d8161c24c
caroquintana <caro.quintana.n@gmail.com>
2017-07-10 13:25:31 -0400

    Etapa4.1:añadiendo los elementos a la clase list-recipe haciendo referencia a su padre cada vez

--- js/script-index.js
+++ js/script-index.js
@@ -1,29 +1,23 @@
 $(document).ready( function(){
-
 	//1.ocultar la flecha
 	$( "a.nav-item.js-back" ).hide();
-
 	//2.añadiendo texto
 	function printNews(){
 		$('.printNews').append("NUEVAS RECETAS");
 	}
 	printNews();
-
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
-
 });
-
 
 /*
 * Función que se encarga de pintar TODAS las recetas que tengan 
 * marcado el atributo "highlighted" como TRUE
 */
-//Etapa4.recorriendo el arreglo con forEach donde le digo que recorra recipesArray y si hightighted es true, me invoque renderRecipe
+//Etapa3.recorriendo el arreglo con forEach donde le digo que recorra recipesArray y si hightighted es true, me invoque renderRecipe
 //Resultado se muestra en consola.
 function renderHighlightedRecipes(recipesArray) {
 	console.log('Recipes: ', recipesArray);
-	
 	var rec = recipesArray.forEach(function(el){
 		// console.log(el.highlighted);
 		if(el.highlighted === true){
@@ -31,7 +25,6 @@ function renderHighlightedRecipes(recipesArray) {
 			renderRecipe(el.highlighted);
 		}
 	});
-
 }
 
 /*
@@ -42,8 +35,17 @@ function renderHighlightedRecipes(recipesArray) {
 */
 function renderRecipe(recipe) {
 	console.log('Voy a pintar la receta: ', recipe);
+	//añadiendo los elementos a la clase list-recipe haciendo referencia a su padre cada vez
+	$(".list-recipes").append("<a class='item-recipe' href='#'></a>");
+	$(".item-recipe").append("<span class='attribution'</span>");
+	$(".attribution").append("<span class='title-recipe'</span>");
+	$(".title-recipe").append("<span class='metadata-recipe'</span>");
+	$(".metadata-recipe").append("<span class='author-recipe'</span>");
+	$(".metadata-recipe").append("<span class='bookmarks-recipe'</span>");
+	$(".bookmarks-recipe").append("<span class='icon-bookmark'</span>");
+	$(".item-recipe").append("<img src='URL DE LA IMAGEN'/>");
 }
-
+renderRecipe(recipe);
 
 
 /*

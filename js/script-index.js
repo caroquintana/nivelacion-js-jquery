475af3900cb22680a2e4ecde3f60c8c711e2ddae
caroquintana <caro.quintana.n@gmail.com>
2017-07-11 00:04:26 -0400

    Etapa5. Oculté el div indicado, me muestra en consola un arr y llamo a la funcion renderActivities arriba porque no me tomaba abajo

--- js/script-index.js
+++ js/script-index.js
@@ -1,4 +1,6 @@
 $(document).ready( function(){
+	//5.llamando a la funcion
+	renderActivities(activitiesArray);
 	//1.ocultar la flecha
 	$( "a.nav-item.js-back" ).hide();
 	//2.añadiendo texto
@@ -6,8 +8,9 @@ $(document).ready( function(){
 		$('.printNews').append("NUEVAS RECETAS");
 	}
 	printNews();
+
 	
-	
+
 	function renderHighlightedRecipes(recipesArray) {
 		//console.log('Recipes: ', recipesArray);
 		var rec = recipesArray.forEach(function(el){
@@ -18,41 +21,41 @@ $(document).ready( function(){
 			};
 		});
 	}
-
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
+	renderActivity(activitiesArray);
+	
 
 });
 
-
-
-/*
-* Función que se encarga de pintar UNA recetas que tenga 
-* marcado el atributo "highlighted" como TRUE
-* Aqui se tiene que crear el HTML que esta en el 
-* archivo "templates/templates-recipe.html"
-*/
 function renderRecipe(recipe) {
-	console.log('Voy a pintar la receta: ', recipe);
-	//añadiendo los elementos a la clase list-recipe haciendo referencia a su padre cada vez
+	//console.log('Voy a pintar la receta: ', recipe);
+	//añadiendo los elementos a la clase list-recipe haciendo referencia a su padre cada vez. Luego invoco al atributo del arreglo para que se muestre.
 	$(".list-recipes").append("<a class='item-recipe' href='#'><span class='attribution'><span class='title-recipe'>" + recipe.title + "</span><span class='metadata-recipe'><span class='author-recipe'>" + recipe.source.name + "</span><span class='bookmarks-recipe'><span class='icon-bookmark'></span></span></span></span><img src='img/recipes/320x350/" + recipe.name + ".jpg'/></a>");
 }
-
 
 /*
 * Función que se encarga de pintar todas las actividades
 */
 function renderActivities(activitiesArray) {
 	console.log('Activities: ', activitiesArray);
-}
+	//ocultar el div indicado
+	if(activitiesArray.length>0){
+		$(".wrapper-message").hide();
+	}
+};
 
 /*
 * Función que se encarga de pintar una actividad
 * Aqui se tiene que crear el HTML que esta en el 
 * archivo "templates/templates-activity.html"
 */
+
 function renderActivity(recipe) {
-	
+
 }
+
+
+
 
 
